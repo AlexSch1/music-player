5c56cc86e973acab414a8a71be17edab860c1097
Aleksey Shulyak <a.shulyak@itransition.com>
2022-10-17 21:17:59 +0400

    add base code

---
+++ src/app/list.js
@@ -0,0 +1,20 @@
+const playlist = [
+    {
+        name: "Onix",
+        artist: "Milo Beats",
+        img: "music-1",
+        src: "MiloBeats-Onix"
+    },
+    {
+        name: "I'll Follow You",
+        artist: "Nothingtosay",
+        img: "music-2",
+        src: "Nothingtosay-IllFollowYou"
+    },
+    {
+        name: "Morning",
+        artist: "Powermitts",
+        img: "music-3",
+        src: "Powermitts-Morning"
+    },
+];
